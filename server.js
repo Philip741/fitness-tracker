5d63a3aab55dc28d1a9143cf2bed1bf401330953
Philip Browning <philip741@gmail.com>
2021-12-17 18:23:21 -0500

    Modified server.js to serve static html pages

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const router = express.Router();
+const path = require("path");
 const PORT = process.env.PORT || 3000;
-
+const routes = require('./routes');
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+module.exports = app; // for testing via mocha/chai

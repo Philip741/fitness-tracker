a157e33ee96de04b6c7175c9afd540e850582f7a
Philip Browning <philip741@gmail.com>
2021-12-29 09:53:31 -0500

    Added route to update exercises. Tested functionality and it is working to add exercises and save new workout.

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -1,23 +1,47 @@
 const router = require('express').Router();
-const fitness = require('../../models/fitness');
+const Workouts = require('../../models/workout');
 
 // get last workout
-router.get('/workouts', (req,res) => {
-        fitness.find({})
-          .sort({ date: -1 })
-          .then(dbTransaction => {
-            res.json(dbTransaction);
-          })
-          .catch(err => {
-            res.status(400).json(err);
-          });
+router.get('/', async (req,res) => {
+        //console.log(Workouts);
+        //allWorkouts = await Workouts.find({});
+        //res.json(allWorkouts);
+         Workouts.find({})
+           .sort({ date: -1 })
+           .then(dbTransaction => {
+             res.json(dbTransaction);
+           })
+           .catch(err => {
+             res.status(400).json(err);
+           });
 });
-
+// Initial workout creation
+router.post('/', (req, res) => {
+    Workouts.create(req.body, (err, data) => {
+    if (err) {
+      console.log(err); 
+    } else {
+      res.json(data);
+    }
+  })
+  })
 // add exercises
+router.put('/:id', (req,res) => {
+   const updateFilter = {_id: req.params.id };
+   const updateExercises = {$push: {exercises: req.body}};
+   Workouts.findOneAndUpdate(updateFilter,updateExercises, function (err, data) {
+        if (err) {
+            res.send(err);
+          } else {
+            res.json(data);
+          }
+    });
+})
 
+module.exports = router;
 // create workout
 
 
 // get workouts in range
 
-router.get('/workouts/range', )
+//router.get('/workouts/range', )
